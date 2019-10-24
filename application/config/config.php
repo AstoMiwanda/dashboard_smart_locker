7660d5b4f9b1eafb41caaddbe60e11290dd21324
AstoMiwanda <astomiwanda08@gmail.com>
2019-10-24 10:50:56 +0700

    cheng base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/dashboard_smart_locker/';
+$config['base_url'] = 'https://dashboard-smartlocker.telkomakses.co.id/';
 
 /*
 |--------------------------------------------------------------------------
